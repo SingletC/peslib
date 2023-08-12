7b34ea592815f004733d5fc4efde12d433a14c88
Chong <chong.teng@bc.edu>
2023-08-11 22:37:39 -0400

    add CI

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-import glob
-import os
 from numpy.distutils.core import setup, Extension
 
 ext_modules = [
     Extension(name='peslibf.o4_singlet', sources=['./src/o4_singlet.f90'], ),
-    Extension(name='peslibf.n4_singlet', sources=['./src/PES_N4_singlet_umn_v3.f90','./src/PES_N4_singlet_umn_v3.pyf'],
+    Extension(name='peslibf.n4_singlet', sources=['./src/PES_N4_singlet_umn_v3.f90', './src/PES_N4_singlet_umn_v3.pyf'],
               ),
-               ]
+]
 if __name__ == "__main__":
     setup(name='peslib',
           packages=['peslib'],
-          ext_modules=ext_modules, )
+          ext_modules=ext_modules,
+          install_requires=[
+              'ase==3.22.1',
+          ]
+
+          )
