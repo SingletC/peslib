b90b17fbcc27b52bb3dbaacadc85a4ba298ece2a
Chong <chong.teng@bc.edu>
2023-10-04 15:17:52 -0400

    - fix bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ ext_modules = [
     Extension(name='peslibf.n4_singlet', sources=['./src/PES_N4_singlet_umn_v3.f90', './src/PES_N4_singlet_umn_v3.pyf'],),
     Extension(name='peslibf.o4_triplet', sources=['./src/O4_triplet_v2.f', './src/O4_triplet_v2.pyf']),
     Extension(name='peslibf.n2o2_triplet', sources=['./src/N2O2_3A_MB-PIP-MEG2.f90', './src/N2O2_3A_MB-PIP-MEG2.pyf'],),
-    Extension(name='peslibf.ch4oh', sources=['./src/ch4oh.pyf', './src/ch4oh.f','./src/lib/utility.f'],),
+    Extension(name='peslibf.ch4oh', sources=['./src/ch4oh.f','./src/lib/utility.f'],),
 ]
 if __name__ == "__main__":
     setup(name='peslib',
