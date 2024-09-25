3be9099dd45e98fea0cff9759791e514614273bb
Chong <chong.teng@bc.edu>
2024-09-25 00:35:51 -0400

    fix bug

--- peslib/pes.py
+++ peslib/pes.py
@@ -231,7 +231,7 @@ class OH3(AdiabaticPES):
         a, ga = self.__pes__func__(r)
         # for now let us just test gs
 
-        return a[self.state] * hatree2ev, - ga[:, :, self.state] * hatree2ev
+        return a[self.state] * hatree2ev, - ga[:, :, self.state] * hatree2ev , None, None # for future use
 
 
 class CH2OH(EvalSurfIO):
@@ -278,8 +278,8 @@ class NH3(AdiabaticPES):
     def _call_method(self, atoms):
         r = atoms.get_positions() * ang2bohr
         u11, u22, u12, v1, v2, gu11, gu22, gu12, gv1, gv2 = self.__pes__func__(r.flatten())
-        r = [(v1 * hatree2ev, -gv1.reshape(-1, 3) * hatree_bohr2ev_ang),
-             (v2 * hatree2ev, -gv2.reshape(-1, 3) * hatree_bohr2ev_ang)]
+        r = [(v1 * hatree2ev, -gv1.reshape(-1, 3) * hatree_bohr2ev_ang, None, None) ,
+             (v2 * hatree2ev, -gv2.reshape(-1, 3) * hatree_bohr2ev_ang, None, None )]
         return r[self.state]
 
 
